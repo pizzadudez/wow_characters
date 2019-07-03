51a5ce9c7b2db6af389cc8c120f02e9f98cf4ece
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-07-04 00:17:18 +0300

    settings file

---
+++ settings.py
@@ -0,0 +1,13 @@
+import json
+
+def set_setting(field):
+    return settings['settings'].get(field, None) or settings['default_settings'].get(field, None)
+    
+with open('settings.json') as f:
+    settings = json.load(f)
+
+
+# characters database
+CHAR_DB = set_setting('char_db')
+LUA_PATHS = set_setting('lua_paths')
+FARMERS = set_setting('farmers')
